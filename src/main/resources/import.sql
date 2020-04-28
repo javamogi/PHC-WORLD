dc3616d1d9f7b224340e8f9f7dd92af7cb24a0d1
javamogi <javamogi@gmail.com>
2020-04-28 12:00:31 +0900

    import.sql FreeBoard UPDATE_DATE 값 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,10 +4,10 @@ insert into USER(ID, EMAIL, PASSWORD, NAME, AUTHORITY, CREATE_DATE, PROFILE_IMAG
 insert into EMAIL_AUTH(ID, EMAIL, AUTH_KEY, AUTHENTICATE) values('1', 'test@test.test', '1234', true);
 insert into EMAIL_AUTH(ID, EMAIL, AUTH_KEY, AUTHENTICATE) values('2', 'test2@test.test', '5678', true);
 
-insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, ICON, BADGE) values('1', '1', 'test', 'test', 0, '2018-07-16 15:55:20.879', '', '');
-insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, ICON, BADGE) values('2', '1', 'test2', 'test2', 0,  '2018-07-17 15:55:20.879', '', '');
-insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, ICON, BADGE) values('3', '1', 'test3', 'test3', 0,  CURRENT_TIMESTAMP(), '', '');
-insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, ICON, BADGE) values('4', '2', 'testtest', 'testtest', 0,  CURRENT_TIMESTAMP(), '', '');
+insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, UPDATE_DATE, ICON, BADGE) values('1', '1', 'test', 'test', 0, '2018-07-16 15:55:20.879', '2018-07-16 15:55:20.879', '', '');
+insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, UPDATE_DATE, ICON, BADGE) values('2', '1', 'test2', 'test2', 0,  '2018-07-17 15:55:20.879', '2018-07-17 15:55:20.879', '', '');
+insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, UPDATE_DATE, ICON, BADGE) values('3', '1', 'test3', 'test3', 0,  CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), '', '');
+insert into FREE_BOARD(ID, WRITER_ID, TITLE, CONTENTS, COUNT, CREATE_DATE, UPDATE_DATE, ICON, BADGE) values('4', '2', 'testtest', 'testtest', 0,  CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), '', '');
 
 insert into TIMELINE(ID, TYPE, ICON, FREE_BOARD_ID, USER_ID, SAVE_DATE) values('1', 'free board', 'list-alt', '1', '1', '2018-07-16 15:55:20.879');
 insert into TIMELINE(ID, TYPE, ICON, FREE_BOARD_ID, USER_ID, SAVE_DATE) values('2', 'free board', 'list-alt', '2', '1', '2018-07-17 15:55:20.879');
