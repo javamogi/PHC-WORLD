bda6e57b90e7d87827d87e3eae5f8a0c58c1022f
Hocheol Park <40781237+javamogi@users.noreply.github.com>
2022-12-17 13:54:47 +0900

    fix: Korean language and table name

--- src/main/java/com/phcworld/domain/user/User.java
+++ src/main/java/com/phcworld/domain/user/User.java
@@ -4,12 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -34,6 +29,7 @@ import lombok.experimental.Accessors;
 @Builder
 @Accessors(chain = true)
 @ToString(exclude = "password")
+@Table(name = "USERS")
 public class User {
 
 	@Id
