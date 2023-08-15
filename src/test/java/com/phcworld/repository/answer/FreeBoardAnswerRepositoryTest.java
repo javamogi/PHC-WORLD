f29e0c081458897f9ccd05fa4e0889257f66bf2a
javamogi <40781237+javamogi@users.noreply.github.com>
2023-08-15 13:35:55 +0900

    test: FreeBoardAnswerRepositoryTest 중복코드 제거

--- src/test/java/com/phcworld/repository/answer/FreeBoardAnswerRepositoryTest.java
+++ src/test/java/com/phcworld/repository/answer/FreeBoardAnswerRepositoryTest.java
@@ -1,14 +1,13 @@
 package com.phcworld.repository.answer;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
+import com.phcworld.domain.answer.FreeBoardAnswer;
+import com.phcworld.domain.board.FreeBoard;
+import com.phcworld.domain.user.User;
+import com.phcworld.repository.board.FreeBoardRepository;
+import com.phcworld.util.FreeBoardFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,38 +15,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.phcworld.domain.answer.FreeBoardAnswer;
-import com.phcworld.domain.board.FreeBoard;
-import com.phcworld.domain.user.User;
+import java.util.List;
+import java.util.Optional;
 
-import lombok.extern.slf4j.Slf4j;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @Transactional
 @Slf4j
 public class FreeBoardAnswerRepositoryTest {
-	
+
+	@Autowired
+	private FreeBoardRepository freeBoardRepository;
+
 	@Autowired
 	private FreeBoardAnswerRepository freeBoardAnswerRepository;
 
+	private User user;
+	private FreeBoard freeBoard;
+
+	@Before
+	public void setup(){
+		user = User.builder()
+				.id(1L)
+				.build();
+		freeBoard = freeBoardRepository.save(FreeBoardFactory.getFreeBoardEntity(user));
+	}
+
 	@Test
 	public void create() {
-		User user = User.builder()
-				.id(1L)
-				.email("user@test.test")
-				.password("user")
-				.name("user")
-				.build();
-		FreeBoard freeBoard = FreeBoard.builder()
-				.id(1L)
-				.writer(user)
-				.title("title")
-				.contents("content")
-				.icon("")
-				.createDate(LocalDateTime.now())
-				.count(0)
-				.build();
 		FreeBoardAnswer answer = FreeBoardAnswer.builder()
 				.writer(user)
 				.freeBoard(freeBoard)
@@ -60,51 +59,18 @@ public class FreeBoardAnswerRepositoryTest {
 	
 	@Test
 	public void read() {
-		User user = User.builder()
-				.id(1L)
-				.email("user@test.test")
-				.password("user")
-				.name("user")
-				.build();
-		FreeBoard freeBoard = FreeBoard.builder()
-				.id(1L)
-				.writer(user)
-				.title("title")
-				.contents("content")
-				.icon("")
-				.createDate(LocalDateTime.now())
-				.count(0)
-				.build();
 		FreeBoardAnswer freeBoardAnswer = FreeBoardAnswer.builder()
 				.writer(user)
 				.freeBoard(freeBoard)
 				.contents("content")
 				.build();
-		freeBoardAnswerRepository.save(freeBoardAnswer);
+		FreeBoardAnswer saveFreeBoardAnswer = freeBoardAnswerRepository.save(freeBoardAnswer);
 		List<FreeBoardAnswer> freeBoardAnswerList = freeBoardAnswerRepository.findByWriter(user);
-		freeBoardAnswerList.stream().forEach(answer -> {
-			log.info("freeBoardAnswer contents : {}", answer.getContents());
-		});
-		assertNotNull(freeBoardAnswerList);
+		assertThat(freeBoardAnswerList).contains(saveFreeBoardAnswer);
 	}
 	
 	@Test
 	public void update() {
-		User user = User.builder()
-				.id(1L)
-				.email("user@test.test")
-				.password("user")
-				.name("user")
-				.build();
-		FreeBoard freeBoard = FreeBoard.builder()
-				.id(1L)
-				.writer(user)
-				.title("title")
-				.contents("content")
-				.icon("")
-				.createDate(LocalDateTime.now())
-				.count(0)
-				.build();
 		FreeBoardAnswer answer = FreeBoardAnswer.builder()
 				.writer(user)
 				.freeBoard(freeBoard)
@@ -114,26 +80,11 @@ public class FreeBoardAnswerRepositoryTest {
 		FreeBoardAnswer registAnswer = freeBoardAnswerRepository.getOne(newAnswer.getId());
 		registAnswer.update("update content");
 		FreeBoardAnswer updatedAnswer = freeBoardAnswerRepository.save(registAnswer);
-		assertThat("update content", is(updatedAnswer.getContents()));
+		assertThat("update content").isEqualTo(updatedAnswer.getContents());
 	}
 	
 	@Test
 	public void delete() {
-		User user = User.builder()
-				.id(1L)
-				.email("user@test.test")
-				.password("user")
-				.name("user")
-				.build();
-		FreeBoard freeBoard = FreeBoard.builder()
-				.id(1L)
-				.writer(user)
-				.title("title")
-				.contents("content")
-				.icon("")
-				.createDate(LocalDateTime.now())
-				.count(0)
-				.build();
 		FreeBoardAnswer answer = FreeBoardAnswer.builder()
 				.writer(user)
 				.freeBoard(freeBoard)
