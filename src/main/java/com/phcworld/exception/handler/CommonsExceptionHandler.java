305540ed01560b7e2dd68d15e3c3408f13f80965
javamogi <40781237+javamogi@users.noreply.github.com>
2023-12-08 11:10:08 +0900

    feat: slack 알림 에러 코드에 추가
    - 500에러가 발생할 경우에 알림

--- src/main/java/com/phcworld/exception/handler/CommonsExceptionHandler.java
+++ src/main/java/com/phcworld/exception/handler/CommonsExceptionHandler.java
@@ -1,31 +1,51 @@
 package com.phcworld.exception.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phcworld.exception.model.CustomBaseException;
 import com.phcworld.exception.model.CustomException;
 import com.phcworld.exception.model.ErrorCode;
 import com.phcworld.exception.model.ErrorResponse;
+import com.phcworld.utils.SlackErrorMessage;
+import com.slack.api.Slack;
+import com.slack.api.webhook.WebhookResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class CommonsExceptionHandler {
 
+    @Value("${notification.slack.webhook.url}")
+    private String slackAlertWebhookUrl;
+
+    private final Slack slack;
+
+    private final ObjectMapper objectMapper;
+
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity handlerException(CustomException e) {
+    public ResponseEntity handlerException(CustomException e) throws IOException {
         Map<String, String> map = e.getMessages();
         if(e.getStatusCode().equals("400")){
             return new ResponseEntity(map, HttpStatus.BAD_REQUEST);
         } else if(e.getStatusCode().equals("401")){
             return new ResponseEntity(map, HttpStatus.UNAUTHORIZED);
         } else if(e.getStatusCode().equals("403")){
+            String errorMessage = e.getMessage();
+            SlackErrorMessage message = new SlackErrorMessage(errorMessage);
+            slack.send(slackAlertWebhookUrl, objectMapper.writeValueAsString(message));
+
             return new ResponseEntity(map, HttpStatus.FORBIDDEN);
         } else if(e.getStatusCode().equals("500")){
             return new ResponseEntity(map, HttpStatus.INTERNAL_SERVER_ERROR);
