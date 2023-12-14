5a6d03e6152e3dd9d0d3a28e566f52b8bdbb4229
javamogi <40781237+javamogi@users.noreply.github.com>
2023-12-14 11:19:02 +0900

    feat: 회원가입 parameter 유효성검사 추가

--- src/main/java/com/phcworld/api/user/dto/UserRequestDto.java
+++ src/main/java/com/phcworld/api/user/dto/UserRequestDto.java
@@ -1,13 +1,11 @@
 package com.phcworld.api.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -17,17 +15,15 @@ import javax.validation.constraints.Size;
 public class UserRequestDto {
 
     @Email(message = "이메일 형식이 아닙니다.")
-    @NotNull
-    @Column(nullable = false, unique = true)
+    @NotBlank(message = "이메일을 입력하세요.")
     private String email;
 
-    @NotNull
-    @Size(min = 4, message = "4자 이상으로 해야합니다.")
-    @JsonIgnore
+    @NotBlank(message = "비밀번호를을 입력하세요.")
+    @Size(min = 4, message = "비밀번호는 4자 이상으로 해야합니다.")
     private String password;
 
-    @NotNull
-    @Size(min = 3, max = 20, message = "영문 3자 이상 20자 이하 또는 한글 두자이상 6자 이하로 해야합니다.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "한글, 영문, 숫자만 가능합니다.")
+    @NotBlank(message = "이름을 입력하세요.")
+    @Size(min = 3, max = 20, message = "이름은 영문 3자 이상 20자 이하 또는 한글 두자이상 6자 이하로 해야합니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "이름은 한글, 영문, 숫자만 가능합니다.")
     private String name;
 }
