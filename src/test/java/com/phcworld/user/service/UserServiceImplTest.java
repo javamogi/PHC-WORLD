dae308cff78abb6bfad9acac07dda6cba40ec178
javamogi <javamogi@gmail.com>
2024-03-31 22:03:57 +0900

    test: 이메일 인증 테스트 추가

--- src/test/java/com/phcworld/user/service/UserServiceImplTest.java
+++ src/test/java/com/phcworld/user/service/UserServiceImplTest.java
@@ -1,13 +1,13 @@
 package com.phcworld.user.service;
 
-import com.phcworld.exception.model.BadRequestException;
-import com.phcworld.exception.model.DuplicationException;
-import com.phcworld.exception.model.NotFoundException;
+import com.phcworld.exception.model.*;
 import com.phcworld.mock.*;
 import com.phcworld.user.domain.Authority;
 import com.phcworld.user.domain.User;
 import com.phcworld.user.domain.UserStatus;
+import com.phcworld.user.domain.dto.LoginRequestUser;
 import com.phcworld.user.domain.dto.UserRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -75,9 +75,9 @@ public class UserServiceImplTest {
         // then
         assertThat(user).isNotNull();
         assertThat(user.getId()).isNotNull();
-        assertThat(user.getEmail()).isEqualTo("test2@test.test");
+        assertThat(user.getEmail()).isEqualTo("pakoh200@test.test");
         assertThat(user.getPassword()).isEqualTo("test2");
-        assertThat(user.getName()).isEqualTo("테스트2");
+        assertThat(user.getName()).isEqualTo("박호철");
         assertThat(user.getCreateDate()).isEqualTo(localDateTime);
         assertThat(user.getAuthority()).isEqualTo(Authority.ROLE_USER);
         assertThat(user.getProfileImage()).isEqualTo("blank-profile-picture.png");
@@ -99,4 +99,47 @@ public class UserServiceImplTest {
         userService.registerUser(request);
     }
 
+    @Test
+    public void 이메일과_인증코드로_회원의_상태_ACTIVE로_변경할_수_있다(){
+        // given
+        String email = "test2@test.test";
+        String certificationCode = "1a2b3c";
+
+        // when
+        User user = userService.verifyCertificationCode(email, certificationCode);
+
+        // then
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getEmail()).isEqualTo("test2@test.test");
+        assertThat(user.getPassword()).isEqualTo("test2");
+        assertThat(user.getName()).isEqualTo("테스트2");
+        assertThat(user.getAuthority()).isEqualTo(Authority.ROLE_USER);
+        assertThat(user.getProfileImage()).isEqualTo("blank-profile-picture.png");
+        assertThat(user.getCertificationCode()).isEqualTo("1a2b3c");
+        assertThat(user.getUserStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void 가입하지_않은_이메일은_예외를_던진다(){
+        // given
+        String email = "temp@test.test";
+        String certificationCode = "1a2b3c";
+
+        // when
+        // then
+        userService.verifyCertificationCode(email, certificationCode);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void 인증코드가_같지_않으면_예외를_던진다(){
+        // given
+        String email = "test2@test.test";
+        String certificationCode = "11111";
+
+        // when
+        // then
+        userService.verifyCertificationCode(email, certificationCode);
+    }
+    
 }
