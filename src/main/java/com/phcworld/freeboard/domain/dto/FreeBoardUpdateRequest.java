fc6081cf8023acdcf0fcb29db476295e84d3dde3
javamogi <javamogi@gmail.com>
2024-04-13 11:46:42 +0900

    refactor: FreeBoardUpdateRequest setter 제거

--- src/main/java/com/phcworld/freeboard/domain/dto/FreeBoardUpdateRequest.java
+++ src/main/java/com/phcworld/freeboard/domain/dto/FreeBoardUpdateRequest.java
@@ -2,11 +2,9 @@ package com.phcworld.freeboard.domain.dto;
 
 import lombok.*;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
-@Setter
 public class FreeBoardUpdateRequest {
 
 	private Long id;
